cfd3a6b901d5c5fe6ca58b5763e3999facefc983
Owethu99Twala <owethutwala99@gmail.com>
2020-07-16 14:56:15 +0200

    index

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+function look(){
+    var arr = [24,8,23,32,5,62].sort((a,b)=> a-b);
+
+    console.log(arr);
+}
+
+
+look();
+
+
+
+
+
+
+
+
+
+// In GitHub create a project with a function that given the following input array [24,8,23,32,5,62] it outputs : [5, 8, 23, 24, 32, 62]. submit link to your GitHub repository 
+
+
